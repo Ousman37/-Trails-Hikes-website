09ddb9242eca4fe0cf16cc944220fad63e8e5832
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-10-03 20:19:18 +0200

    test

--- js/appis.js
+++ js/appis.js
@@ -5,3 +5,41 @@ faqs.forEach((faq) => {
     faq.classList.toggle("active");
   });
 });
+
+const nameError = document.getElementById("name-error");
+const subjectError = document.getElementById("subject-error");
+const emailError = document.getElementById("email-error");
+const messageError = document.getElementById("message-error");
+const submitError = document.getElementById("submit-error");
+
+function validateName() {
+  const name = document.getElementById("contact-name").value;
+
+  if (name.length === 0) {
+    nameError.innerHTML = "We need your first name, it’s nicer that way";
+    return false;
+  }
+  if (!name.match(/∧[A-Za-z]*\s{}[A-Za-z]*$/)) {
+    nameError.innerHTML = "Write full name";
+    return false;
+  }
+  nameError.innerHTML = `<i class="ri-checkbox-circle-fill"></i>`;
+  return true;
+}
+
+function validateSubject() {
+  const subject = document.getElementById("contact-subject").value;
+
+  if (subject.length === 0) {
+    subjectError.innerHTML = `your subject is required.`;
+    return false;
+  }
+  if (subject.length !== 10) {
+    subjectError.innerHTML = `your subject is required.`;
+    return false;
+  }
+  if (subject.length === 0) {
+    subjectError.innerHTML = `your subject is required.`;
+    return false;
+  }
+}
